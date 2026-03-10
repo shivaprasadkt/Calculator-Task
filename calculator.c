ac1b66de297f44e38a50a43f52e757d11653fa35
shivaprasadkt <shiva2215685@gmail.com>
2026-03-10 21:51:00 +0530

    Added division function

--- calculator.c
+++ calculator.c
@@ -11,3 +11,11 @@ int Subnum(int a,int b){
 int Mulnum(int a,int b){
     return a*b;
 }
+
+int Divnum(int a,int b){
+    if(b!=0){
+        return a/b;
+    }
+    printf("Division by zero is not possible!!!");
+    return ;
+}

60bccd98c66c767c089f87b46b531ff679117fe4
shivaprasadkt <shiva2215685@gmail.com>
2026-03-10 22:37:29 +0530

    Added main function

--- calculator.c
+++ calculator.c
@@ -1,4 +1,4 @@
-#include<Stdio.h>
+#include<stdio.h>
 #include<math.h>
 
 int Addnum(int a,int b){
@@ -16,13 +16,12 @@ int Mulnum(int a,int b){
     return res;
 }
 
-int Divnum(int a,int b){
+float Divnum(int a,int b){
     if(b!=0){
-        float res=a/b;
-        return res;
+        return (float)a/b;
     }
-    printf("Division by zero is not possible!!!");
-    return ;
+    printf("Division by zero is not possible!!!\n");
+    return 0;
 }
 
 int Expnum(int a,int b){
@@ -30,9 +29,39 @@ int Expnum(int a,int b){
     return res;
 }
 
-int Rootnum(int a,int b){
+void Rootnum(int a,int b){
     float res1=sqrt(a);
     float res2=sqrt(b);
-    printf("Square root of number 1=%d",res1);
-    printf("Square root of number 2=%d",res2);
+    printf("Square root of number 1=%f\n",res1);
+    printf("Square root of number 2=%f\n\n",res2);
+}
+
+int main(){
+    int n,a,b;
+    do{
+        printf("------MENU------\n");
+        printf("1.Addition\n2.Subtraction\n3.Multiplication\n4.Division\n5.Exponent\n6.Square root\n7.Exit\n\n");
+        printf("Enter the choice[1-7]:");
+        scanf("%d",&n);
+        printf("Enter the 2 numbers:");
+        scanf("%d %d",&a,&b);
+        printf("\n");
+        switch(n){
+            case 1:printf("The sum is=%d\n\n",Addnum(a,b));
+            break;
+            case 2:printf("The difference is=%d\n\n",Subnum(a,b));
+            break;
+            case 3:printf("The product is=%d\n\n",Mulnum(a,b));
+            break;
+            case 4:printf("The result is=%f\n\n",Divnum(a,b));
+            break;
+            case 5:printf("The result is=%d\n\n",Expnum(a,b));
+            break;
+            case 6:Rootnum(a,b);
+            break;
+            case 7:printf("Exiting the program......\n");
+            break;
+            default:printf("Invalid function!!!\n\n");
+        }
+    }while(n!=7);
 }

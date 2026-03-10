fb32a57144b3f5f7219de3243e23498b47a1c5a2
shivaprasadkt <shiva2215685@gmail.com>
2026-03-10 22:03:03 +0530

    Added square root function

--- calculator.c
+++ calculator.c
@@ -2,25 +2,37 @@
 #include<math.h>
 
 int Addnum(int a,int b){
-    return a+b;
+    int res=a+b;
+    return res;
 }
 
 int Subnum(int a,int b){
-    return a-b;
+    int res=a-b;
+    return res;
 }
 
 int Mulnum(int a,int b){
-    return a*b;
+    int res=a*b;
+    return res;
 }
 
 int Divnum(int a,int b){
     if(b!=0){
-        return a/b;
+        float res=a/b;
+        return res;
     }
     printf("Division by zero is not possible!!!");
     return ;
 }
 
 int Expnum(int a,int b){
-    return pow(a,b);
+    int res=pow(a,b);
+    return res;
+}
+
+int Rootnum(int a,int b){
+    float res1=sqrt(a);
+    float res2=sqrt(b);
+    printf("Square root of number 1=%d",res1);
+    printf("Square root of number 2=%d",res2);
 }
